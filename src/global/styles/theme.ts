e5c512c8c92d455a0d304c4a0427e5fe8aa7291a
Yang Silva <yang.silva@gmail.com>
2022-06-25 12:20:57 -0300

    Acrescentando ícones ao Header

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -11,9 +11,18 @@ export default {
     attention: '#E83F5B',
     attention_light: 'rgba(232,63, 91, 0.5)',
 
-    shape: 'FFFFFF',
+    shape: '#FFFFFF',
     title: '#363F5F',
     text: '#969CB2',
     background: '#F0F2F5',
+  },
+
+  fonts: {
+
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
   }
+
 }
+
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  Icon
+  } from './styles';
 
 export function Dashboard() {
   return (
       <Container>
-        <Title>Dashboard</Title>
+        <Header>
+        <UserWrapper>
+          <UserInfo>
+            {/* IMAGEM RETIRADA DO PERFIL DO GOOGLE */}
+            <Photo
+            source=
+            {{ uri: 'https://lh3.googleusercontent.com/a-/AOh14Gg3fIq24Il6CTZikB9WKIHQWwU8AvMNXoso3fw2RQ=s96-c-rg-br100'}}
+            />
+              <User>
+                <UserGretting>Olá,</UserGretting>
+                <UserName>Yang</UserName>
+                </User>
+          </UserInfo>
+          <Icon name="power" />
+          </UserWrapper>
+        </Header>
       </Container>
 
   )
