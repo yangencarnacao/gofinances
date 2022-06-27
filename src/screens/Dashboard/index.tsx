2e167ae7ada01318c0252de5741b0b3af72a8d1d
Yang Silva <yang.silva@gmail.com>
2022-06-27 20:31:59 -0300

    Posicionando cards - início

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  Icon,
+  HighlightCards
+  } from './styles';
+
+export function Dashboard() {
+  return (
+      <Container>
+        <Header>
+        <UserWrapper>
+          <UserInfo>
+            {/* IMAGEM RETIRADA DO PERFIL DO GOOGLE */}
+            <Photo
+            source=
+            {{ uri: 'https://lh3.googleusercontent.com/a-/AOh14Gg3fIq24Il6CTZikB9WKIHQWwU8AvMNXoso3fw2RQ=s96-c-rg-br100'}}
+            />
+              <User>
+                <UserGretting>Olá,</UserGretting>
+                <UserName>Yang</UserName>
+                </User>
+          </UserInfo>
+          <Icon name="power" />
+          </UserWrapper>
+        </Header>
+
+        <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+        </HighlightCards>
+
+      </Container>
+
+  )
+}
+
