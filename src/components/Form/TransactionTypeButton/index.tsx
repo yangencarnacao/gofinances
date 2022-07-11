2a776808cd31106eb510f51a064c5f8c7592474d
Yang Silva <yang.silva@gmail.com>
2022-07-11 07:16:05 -0300

    Criando o Componente para selecionar o tipo da transação

--- src/components/Form/TransactionTypeButton/index.tsx
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import { TouchableOpacityProps } from 'react-native';
 
-import { 
+import {
     Container,
     Icon,
     Title,
  } from './styles';
 
+const icons = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle'
+}
+
 interface Props extends TouchableOpacityProps {
-    title: string;
-    type: 'up' | 'down';
+  type: 'up' | 'down';
+  title: string;
+  isActive: boolean;
 }
 
 export function TransactionTypeButton ({
+    type,
     title,
+    isActive,
     ...rest
 } : Props) {
     return (
-    <Container {...rest}>    
-    <Icon />
+    <Container
+    isActive={isActive}
+    type={type}
+    {...rest}
+    >
+    <Icon
+    name={icons[type]}
+    type={type}
+    />
     <Title>
         { title }
     </Title>
     </Container>
-    
-    );     
+
+    );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 
 
-import { 
+import {
     Container,
     Header,
     Title,
     Form,
     Fields,
+    TransactionsType,
  } from './styles';
 
 export function Register () {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down'){
+      handleTransactionsTypeSelect(type);
+    }
+
     return (
     <Container>
         <Header>
@@ -19,17 +27,31 @@ export function Register () {
         </Header>
         <Form>
         <Fields>
-        <Input 
+        <Input
         placeholder='Nome'
         />
-        <Input 
+        <Input
         placeholder='Preço'
         />
+        <TransactionsType>
+        <TransactionTypeButton
+          type="up"
+          title="Income"
+          onPress={() => handleTransactionsTypeSelect('up')}
+          isActive={transactionType === 'up'}
+        />
+        <TransactionTypeButton
+          type="down"
+          title="Outcome"
+          onPress={() => handleTransactionsTypeSelect('down')}
+          isActive={transactionType === 'down'}
+        />
+        </TransactionsType>
         </Fields>
 
         <Button title='Enviar' />
         </Form>
 
     </Container>
-    );     
+    );
 }
